8e335bd252c662aad710762c7a89c55f1a0e1733
Anonymous <sipayi@gmail.com>
2020-05-28 18:07:53 -0400

    Bellman-Ford practice

--- atcoder/practice/abc137E.java
+++ atcoder/practice/abc137E.java
@@ -2,10 +2,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-
+ 
 public class abc137E {
 	static long INF = (long) 1e9 + 7;
-
+ 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String[] tokens = br.readLine().split(" ");
@@ -17,24 +17,23 @@ public class abc137E {
 			tokens = br.readLine().split(" ");
 			int src = Integer.parseInt(tokens[0]) - 1;
 			int dest = Integer.parseInt(tokens[1]) - 1;
-			int weight = Integer.parseInt(tokens[2]);
+			int weight = Integer.parseInt(tokens[2]) - P;
 			edges[i] = new Edge(src, dest, weight);
 		}
 		long[] dist = new long[N];
 		Arrays.fill(dist, -INF);
 		dist[0] = 0;
-		long max = 0;
-		for (int i = 1; i < N; i++) {
-			long[] curr = new long[N];
-			Arrays.fill(curr, -INF);
+		for (int i = 0; i < N; i++) {
+//			System.out.println(Arrays.toString(dist));
+			boolean done = true;
 			for (Edge e : edges) {
-				curr[e.v] = Math.max(curr[e.v], dist[e.u] + e.w);
+				if (dist[e.v] < dist[e.u] + e.w) {
+					dist[e.v] = dist[e.u] + e.w;
+					done = false;
+				}
 			}
-			if (curr[N - 1] != INF)
-				max = Math.max(max, curr[N - 1] - P * i);
-			for (int v = 0; v < N; v++) {
-				dist[v] = Math.max(dist[v], curr[v]);
-			}
+			if (done)
+				break;
 		}
 //		System.out.println(Arrays.toString(dist));
 		for (Edge e : edges) {
@@ -44,26 +43,26 @@ public class abc137E {
 					return;
 				}
 		}
-		System.out.println(dist[N - 1] == INF ? -1 : max);
+		System.out.println(dist[N - 1] == INF ? -1 : Math.max(0, dist[N - 1]));
 	}
-
+ 
 	static class Edge {
 		int u;
 		int v;
 		int w;
-
+ 
 		Edge(int src, int dest, int w) {
 			u = src;
 			v = dest;
 			this.w = w;
 		}
-
+ 
 		Edge(Edge e) {
 			u = e.u;
 			v = e.v;
 			w = e.w;
 		}
-
+ 
 		@Override
 		public String toString() {
 			return String.format("[%d, %d, %d]", u, v, w);
