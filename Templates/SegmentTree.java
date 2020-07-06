fb927aae62f6612b5bc4e683276aff1743077cf1
Anonymous <sipayi@gmail.com>
2020-07-06 14:58:33 -0400

    Added Generics to Segment Tree

--- Templates/SegmentTree.java
+++ Templates/SegmentTree.java
@@ -1,40 +1,38 @@
-import java.util.Comparator;
+public class SegmentTree<T> {
 
-public class SegmentTree {
-
-	private int N;
-	public Integer[] segtree;
-	public int[] input;
-	Comparator<Integer> comparator;
-
-	public SegmentTree(int[] input) {
-		this(input, (a, b) -> Math.max(a, b));// default to Max Segment Tree
+	public static interface Combiner<T> {
+		T combine(T l, T r);
 	}
 
-	public SegmentTree(int[] arr, Comparator<Integer> c) {
+	private int N;
+	public T[] segtree;
+	public T[] input;
+	Combiner<T> combiner;
+
+	public SegmentTree(T[] arr, Combiner<T> c) {
 		/*
 		 * Specify Type of Segment Tree with Comparator E.G.: Min -> (a, b) ->
 		 * Math.min(a, b) Sum -> (a, b) -> a + b ...
 		 */
 		N = arr.length;
-		segtree = new Integer[4 * N];
+		segtree = (T[]) new Object[4 * N];
 		input = arr;
-		comparator = c;
+		combiner = c;
 		createTree(input, 0, 0, N - 1);
 	}
 
-	private void createTree(int[] arr, int node, int start, int end) {
+	private void createTree(T[] arr, int node, int start, int end) {
 		if (start == end) {
 			segtree[node] = arr[start];
 		} else {
 			int mid = (start + end) / 2;
 			createTree(arr, 2 * node + 1, start, mid);
 			createTree(arr, 2 * node + 2, mid + 1, end);
-			segtree[node] = comparator.compare(segtree[2 * node + 1], segtree[2 * node + 2]);
+			segtree[node] = combiner.combine(segtree[2 * node + 1], segtree[2 * node + 2]);
 		}
 	}
 
-	private void update(int idx, int l, int r, int i, int v) {
+	private void update(int idx, int l, int r, int i, T v) {
 		if (l == r)
 			segtree[idx] = v;
 		else {
@@ -43,31 +41,31 @@ public class SegmentTree {
 				update(2 * idx + 1, l, m, i, v);
 			else
 				update(2 * idx + 2, m + 1, r, i, v);
-			segtree[idx] = comparator.compare(segtree[2 * idx + 1], segtree[2 * idx + 2]);
+			segtree[idx] = combiner.combine(segtree[2 * idx + 1], segtree[2 * idx + 2]);
 		}
 	}
 
-	public void update(int index, int newValue) {
+	public void update(int index, T newValue) {
 		update(0, 0, N - 1, index, newValue);
 	}
 
-	private int get(int idx, int l, int r, int lhs, int rhs) {
+	private T get(int idx, int l, int r, int lhs, int rhs) {
 		if (l >= lhs && r <= rhs)
 			return segtree[idx];
-		Integer ret = null;
+		T ret = null;
 		int m = (l + r) / 2;
 		if (m >= lhs)
-			ret = compare(ret, get(2 * idx + 1, l, m, lhs, rhs));
+			ret = combine(ret, get(2 * idx + 1, l, m, lhs, rhs));
 		if (m + 1 <= rhs)
-			ret = compare(ret, get(2 * idx + 2, m + 1, r, lhs, rhs));
+			ret = combine(ret, get(2 * idx + 2, m + 1, r, lhs, rhs));
 		return ret;
 	}
 
-	public int get(int l, int r) {
+	public T get(int l, int r) {
 		return get(0, 0, N - 1, l, r);
 	}
 
-	public Integer compare(Integer a, Integer b) {
+	public T combine(T a, T b) {
 		if (a == null && b == null) {
 			return null;
 		}
@@ -77,6 +75,7 @@ public class SegmentTree {
 		if (b == null) {
 			return a;
 		}
-		return comparator.compare(a, b);
+		return combiner.combine(a, b);
 	}
+
 }
