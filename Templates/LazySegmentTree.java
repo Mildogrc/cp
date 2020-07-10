798dcb3c795430fd420bbf5c4ee65b5bd45a17af
Anonymous <sipayi@gmail.com>
2020-07-10 03:57:51 -0400

    LazySegmentTree 1.1 (Not functional)

--- Templates/LazySegmentTree.java
+++ Templates/LazySegmentTree.java
@@ -1,26 +1,27 @@
-import java.util.Arrays;
-import java.util.Comparator;
-
-public class LazySegmentTree {
+public class LazySegmentTree<T, K> {
+	public static interface Combiner<T, K> {
+		T combine(T l, K r);
+	}
 	int N;
-	public int[] segTree;
-	public int[] lazy;
-	public int[] input;
-	public Comparator<Integer> comparator;
-	public int DEFAULT;
+	public T[] segTree;
+	public K[] lazy;
+	public T[] input;
+	public Combiner<T, T> c;
+	public Combiner<K, K> push;
+	public Combiner<T, K> fix;
 
-	LazySegmentTree(int[] input) {
-		this(input, (a, b) -> Math.max(a, b), Integer.MIN_VALUE);
+	LazySegmentTree(int N, Combiner<T, T> c, Combiner<K, K> push, Combiner<T, K> fix) {
+		this((T[]) new Object[N], c, push, fix);
 	}
 
-	LazySegmentTree(int[] input, Comparator<Integer> c, int DEFAULT) {
+	LazySegmentTree(T[] input, Combiner<T, T> combinerup, Combiner<K, K> combinerdown, Combiner<T, K> fix) {
 		N = input.length;
-		segTree = new int[4 * N];
-		Arrays.fill(segTree, DEFAULT);
-		lazy = new int[4 * N];
+		segTree = (T[]) new Object[N << 2];
+		lazy = (K[]) new Object[N << 2];
 		this.input = input;
-		comparator = c;
-		this.DEFAULT = DEFAULT;
+		c = combinerup;
+		push = combinerdown;
+		this.fix = fix;
 		createTree(0, N - 1, 0);
 	}
 
@@ -32,28 +33,28 @@ public class LazySegmentTree {
 		int mid = (lhs + rhs) / 2;
 		createTree(lhs, mid, 2 * node + 1);
 		createTree(mid + 1, rhs, 2 * node + 2);
-		segTree[node] = comparator.compare(segTree[2 * node + 1], segTree[2 * node + 2]);
+		segTree[node] = combine(segTree[2 * node + 1], segTree[2 * node + 2]);
 	}
 
-	public void update(int node, int change) {
+	public void update(int node, K change) {
 		update(node, node, change);
 	}
 
-	public void update(int start, int end, int change) {
+	public void update(int start, int end, K change) {
 		update(start, end, change, 0, input.length - 1, 0);
 	}
 
-	private void update(int start, int end, int change, int lhs, int rhs, int node) {
+	private void update(int start, int end, K change, int lhs, int rhs, int node) {
 		if (lhs > rhs) {
 			return;
 		}
-		if (lazy[node] != 0) {
-			segTree[node] += lazy[node];
+		if (lazy[node] != null) {
+			segTree[node] = fix.combine(segTree[node], lazy[node]);
 			if (lhs != rhs) {
-				lazy[2 * node + 1] += lazy[node];
-				lazy[2 * node + 2] += lazy[node];
+				lazy[2 * node + 1] = push.combine(lazy[2 * node + 1], lazy[node]);
+				lazy[2 * node + 2] = push.combine(lazy[2 * node + 2], lazy[node]);
 			}
-			lazy[node] = 0;
+			lazy[node] = null;
 		}
 
 		if (start > rhs || end < lhs) {
@@ -61,39 +62,39 @@ public class LazySegmentTree {
 		}
 
 		if (start <= lhs && end >= rhs) {
-			segTree[node] += change;
+			segTree[node] = fix.combine(segTree[node], change);
 			if (lhs != rhs) {
-				lazy[2 * node + 1] += change;
-				lazy[2 * node + 2] += change;
+				lazy[2 * node + 1] = push.combine(lazy[2 * node + 1], change);
+				lazy[2 * node + 2] = push.combine(lazy[2 * node + 2], change);
 			}
-			lazy[node] = 0;
+			lazy[node] = null;
 		}
 
 		int mid = (lhs + rhs) / 2;
 		update(start, end, change, lhs, mid, 2 * node + 1);
 		update(start, end, change, mid + 1, rhs, 2 * node + 2);
-		segTree[node] = comparator.compare(segTree[2 * node + 1], segTree[2 * node + 2]);
+		segTree[node] = combine(segTree[2 * node + 1], segTree[2 * node + 2]);
 	}
 
-	public int get(int l, int r) {
+	public T get(int l, int r) {
 		return get(l, r, 0, N - 1, 0);
 	}
 
-	private int get(int l, int r, int lhs, int rhs, int idx) {
+	private T get(int l, int r, int lhs, int rhs, int idx) {
 		if (lhs > rhs) {
-			return DEFAULT;
+			return null;
 		}
-		if (lazy[idx] != 0) {
-			segTree[idx] += lazy[idx];
+		if (lazy[idx] != null) {
+			segTree[idx] = fix.combine(segTree[idx], lazy[idx]);
 			if (lhs != rhs) {
-				lazy[2 * idx + 1] += lazy[idx];
-				lazy[2 * idx + 2] += lazy[idx];
+				lazy[2 * idx + 1] = push.combine(lazy[2 * idx + 1], lazy[idx]);
+				lazy[2 * idx + 2] = push.combine(lazy[2 * idx + 2], lazy[idx]);
 			}
-			lazy[idx] = 0;
+			lazy[idx] = null;
 		}
 
 		if (l > rhs || r < lhs) {
-			return DEFAULT;
+			return null;
 		}
 
 		if (l <= lhs && r >= rhs) {
@@ -101,7 +102,21 @@ public class LazySegmentTree {
 		}
 
 		int mid = (lhs + rhs) / 2;
-		return comparator.compare(get(l, r, lhs, mid, 2 * idx + 1), get(l, r, mid + 1, rhs, 2 * idx + 2));
+		return combine(get(l, r, lhs, mid, 2 * idx + 1), get(l, r, mid + 1, rhs, 2 * idx + 2));
 
 	}
+
+	public T combine(T a, T b) {
+		if (a == null && b == null) {
+			return null;
+		}
+		if (a == null) {
+			return b;
+		}
+		if (b == null) {
+			return a;
+		}
+		return c.combine(a, b);
+	}
+
 }
