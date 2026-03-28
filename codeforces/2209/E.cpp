d88de6d5d19f4ed8b8bce94b3522f97adf4eced0
Milind Chandramohan <sipayi@gmail.com>
2026-03-28 10:41:39 -0400

    saving bevore Nebius Div. 1+2 comp

--- codeforces/2209/E.cpp
+++ codeforces/2209/E.cpp
@@ -2,20 +2,26 @@
 using namespace std;
 #define int long long
 
+int f(string x, int l, int r) {
+    int sum = 0;
+    int c = 1;
+    int j = l;
+    for (int i = l; i <= r; i++) {
+        if(x[i] == x[j]) {
+            c++;
+        } else if (x[i] )
+    }
+}
+
 void solve() {
     int n, q;
-    cin >> n >> q;
     string s;
+    cin >> n >> q;
     cin >> s;
     while(q--) {
         int l, r;
         cin >> l >> r;
-        int l1 = l;
-        int r1 = l;
-        int c = 1;
-        for(int i = l; l < r; i++) {
-            
-        }
+        cout << f(s, l-1, r-1) << "\n";
     }
 }
 
---
+++ codeforces/2211/B.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+// #define int long long
+
+void solve() {
+
+}
+
+signed main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    int t; cin >> t; while (t--) solve();
+}
